f8659401ce749f9b5d7d647657e058b062284829
kratos619 <gaurav.palaspagar@hotmail.com>
2018-07-06 20:41:52 +0530

    Creating Dynamic Layoutes

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,17 +1,5 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-    </head>
-    <body>
-<h1>This is contact Page {{$id}}</h1>
-    </body>
-</html>
+@section('content')
+<h1>Post id = {{$id}}</h1>
+@stop

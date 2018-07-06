154f3bc9eab70b5587883c1ff2fb4af49d267993
kratos619 <gaurav.palaspagar@hotmail.com>
2018-07-06 19:06:46 +0530

    passing data to Controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,9 +11,9 @@ class PostController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index($id)
     {
-        return "HelloThis is PostController";
+        return "HelloThis is PostController id is = " . $id;
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/post','PostController@index');
+Route::get('/post/{id}','PostController@index');
